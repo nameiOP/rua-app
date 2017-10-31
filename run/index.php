015271a3b8c5aca6bcf455b3d2c8526f622ff758
wyy <wyy@mac.com>
2017-10-31 15:25:06 +0800

    init

--- run/index.php
+++ run/index.php
@@ -1,3 +1,14 @@
 <?php
 
 defined('RUA_DEBUG') or define('RUA_DEBUG',true);
+defined('RUA_ENV') or define('RUA_ENV','dev');
+defined('RUA_ENV_DEV') or define('RUA_ENV_DEV', true);
+
+
+//引入框架文件
+require(__DIR__ . '/../vendor/rua/framework/Builder.php');
+
+
+//run
+$config = require(__DIR__ . '/../config/cli.php');
+(new rua\cli\app($config))->run();
